2f43ef9c3b3aaab80bc764c0bb306d3b2016cc20
Christian Garcia <christian.garcia@origamibits.co.uk>
2021-04-09 12:53:57 +0100

    Blank

--- app/src/main/java/uk/co/origamibits/bootstrap/IoDispatcher.kt
+++
@@ -1,6 +0,0 @@
-package uk.co.origamibits.bootstrap
-
-import javax.inject.Qualifier
-
-@Qualifier
-annotation class IoDispatcher
--- app/src/main/java/uk/co/origamibits/bootstrap/MainActivity.kt
+++ app/src/main/java/uk/co/origamibits/bootstrap/MainActivity.kt
@@ -1,13 +1,8 @@
 package uk.co.origamibits.bootstrap
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import dagger.hilt.android.AndroidEntryPoint
-import uk.co.origamibits.bootstrap.databinding.ActivityMainBinding
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
